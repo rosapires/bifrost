a3a57c3ab2ef672faa28b9de49e82b9c79a8324b
Birkir Björnsson <birkirbjornsson@Birkirs-MacBook-Pro.local>
2018-12-07 21:35:29 +0100

    new js

--- js/script.js
+++ js/script.js
@@ -1,72 +1,25 @@
-let categories_link = "http://tabithabjorkman.com/bifrost_t/json/categories.php";
-let categoryTemplate = document.querySelector('.categoryTemplate').content;
-let main = document.querySelector('main');
-let skills_link = "http://tabithabjorkman.com/bifrost_t/json/skills.php";
-let skillsTemplate = document.querySelector('.skillsTemplate').content;
-
-
-function getCategories(catlink) {
-    //console.log(catlink);
-    fetch(catlink)
-        .then(function (response) {
-            return response.json()
-        })
-        .then(function (data) {
-            data.forEach(createCategory);
-            getData(categories_link);
-        })
-
+"use strict"
+function getData(){
+    fetch("http://tabithabjorkman.com/bifrost_t/json/categories.php")
+    //fetch("http://tabithabjorkman.com/bifrost_t/json/skills.php")
+    .then(res => res.json())
+    .then(showCategories)
+    //.then(showSkills);
+   
 }
+getData()
 
-function createCategory(cat) {
-    console.log(cat)
-    let clone = categoryTemplate.cloneNode(true);
-    clone.querySelector('h1').textContent = cat;
+const catTemp = document.querySelector (".categoryTemplate").content; 
 
-    main.appendChild(clone);
-}
-
-function getData(link) {
-    fetch(link).then(function (response) {
-        return response.json();
-    }).then(function (json) {
-        return show(json);
+function showCategories(data){
+    console.log(data);
+        data.forEach(cat => {
+            console.log(cat)
+        const clone = catTemp.cloneNode(true);
+        clone.querySelector(".name").textcontent = cat.category_name;
+        console.log();
+      
+        document.querySelector("main").appendChild(clone);
+        
     });
 }
-
-
-function show(json) {
-    json.forEach(function (cat) {
-        let clone = categoryTemplate.cloneNode(true);
-        clone.querySelector('.name').textContent = category_name;
-
-
-
-
-    });
-}
-
-getCategories(categories_link)
-
-function getSkills(skillslink) {
-    //console.log(catlink);
-    fetch(skillslink)
-        .then(function (response) {
-            return response.json()
-        })
-        .then(function (data) {
-            data.forEach(createSkills);
-            getData(skills_link);
-        })
-
-}
-
-function createSkills(skills) {
-    console.log(skills)
-    let clone = skillsTemplate.cloneNode(true);
-    clone.querySelector('.skill').textContent = skills;
-
-    div.appendChild(clone);
-}
-
-getSkills(skills_link)
